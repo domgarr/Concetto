25b70590a79d4dda8bbe5896c2c51ef450a0b314
unknown <garreffd@uwindsor.ca>
2020-01-23 14:23:21 -0500

    restructured project to include backend

---
+++ src/main/java/com/example/concetto/ConcettoApplication.java
@@ -0,0 +1,13 @@
+package com.example.concetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcettoApplication.class, args);
+	}
+
+}
