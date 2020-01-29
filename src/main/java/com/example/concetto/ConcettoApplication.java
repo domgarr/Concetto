2bbe58eb0243ab8521198fd1450a6f1e1e510483
unknown <garreffd@uwindsor.ca>
2020-01-29 14:48:11 -0500

    Add social login (facebook, google) config in backend

--- src/main/java/com/example/concetto/ConcettoApplication.java
+++ src/main/java/com/example/concetto/ConcettoApplication.java
@@ -1,13 +1,126 @@
 package com.example.concetto;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
 
 @SpringBootApplication
-public class ConcettoApplication {
-
+@EnableOAuth2Client
+@RestController
+public class ConcettoApplication extends WebSecurityConfigurerAdapter {	
 	public static void main(String[] args) {
 		SpringApplication.run(ConcettoApplication.class, args);
 	}
+	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+	
+	@Autowired 
+	OAuth2ClientContext oAuth2ClientContext;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**")
+	    .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
+		.authorizeRequests()
+		.antMatchers("/", "/login/**", "/error/**", "/webjars/**")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.logout().logoutSuccessUrl("/").permitAll()
+	    .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 
+	}
+	
+	//Supports redirect from our app to Facebook, Google.
+	@Bean
+	public FilterRegistrationBean<OAuth2ClientContextFilter> oAuth2ClientfilterRegistration(OAuth2ClientContextFilter filter){
+		FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
+		registration.setFilter(filter);
+		registration.setOrder(-100); //Use lower order to ensure that it comes before Spring Security Filter.
+		return registration;
+		
+	}
+	
+	//https://spring.io/guides/tutorials/spring-boot-oauth2/#_social_login_simple
+	//Everything done below is auto-magically configured by using @EnableOAuth2Sso but it is deprecated.
+	private Filter ssoFilter() {
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+		
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(),oAuth2ClientContext );
+		facebookFilter.setRestTemplate(facebookTemplate);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebookResource().getClientId());
+		tokenServices.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(tokenServices);
+		filters.add(facebookFilter);
+		
+		OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/google");
+		OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google(), oAuth2ClientContext );
+		googleFilter.setRestTemplate(googleTemplate);
+		tokenServices = new UserInfoTokenServices(googleResource().getUserInfoUri(), googleResource().getClientId());
+		tokenServices.setRestTemplate(googleTemplate);
+		googleFilter.setTokenServices(tokenServices);
+		filters.add(googleFilter);
+		
+		filter.setFilters(filters);
+		return filter;
+	}
+	
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	public AuthorizationCodeResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+	
+	//Contains info of user info endpoint 
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	public ResourceServerProperties facebookResource() {
+	    return new ResourceServerProperties();
+	}
+	
+	@Bean
+	@ConfigurationProperties("google.client")
+	public AuthorizationCodeResourceDetails google() {
+		return new AuthorizationCodeResourceDetails();
+	}
+	
+	@Bean
+	@ConfigurationProperties("google.resource")
+	public ResourceServerProperties googleResource() {
+		return new ResourceServerProperties();
+	}
 }
---
+++ src/main/java/com/example/concetto/Test.java
@@ -0,0 +1,5 @@
+package com.example.concetto;
+
+public class Test {
+
+}
