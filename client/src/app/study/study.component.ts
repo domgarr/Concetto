f9586a8ff749b7e2790a669ae989da0da6a25846
Domenico Garreffa <garreffd92@gmail.com>
2020-06-19 22:29:00 -0400

    add from study now works, fix template error and refactor view-concept

--- client/src/app/study/study.component.ts
+++ client/src/app/study/study.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { Concept } from '../models/concept';
 import { ConceptService } from '../services/concept.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SubjectService } from '../services/subject.service';
 import { Subject } from '../models/subject';
 
@@ -18,29 +18,33 @@ private index : number;
 concepts : Concept[];
   
 
-  constructor(private conceptService : ConceptService, private subjectService : SubjectService, private activatedRoute : ActivatedRoute) {
+  constructor(private conceptService : ConceptService, private subjectService : SubjectService, private activatedRoute : ActivatedRoute, private router : Router) {
     this.index = 0;
+    this.subject = new Subject();
    }
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe(params =>{
       let subjectId : number = Number(params.get("subject_id"));
+      //TODO: Load in data using AuthGuard?
       this.conceptService.getAllConceptsBySubjectId(subjectId).subscribe(concepts =>{
         this.concepts = concepts;
         this.concept = concepts[this.index];
       });
       this.subjectService.getSubject(subjectId).subscribe(subject =>{
         this.subject = subject;
-        console.log(this.subject);
       });
     });
   }
 
-  onGoodPressed(){
+  onGoodPressed(event){
     this.index++;
     if(this.index < this.concepts.length){
     this.concept = this.concepts[this.index];
-    console.log(this.index);
     }
+  }
+
+  onAddButtonPress(){
+    this.router.navigate(['/u/add-concept', this.subject.id]);
   }
 }
--- client/src/app/view-concept/view-concept.component.ts
+++ client/src/app/view-concept/view-concept.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Concept} from '../models/concept';
 import { ConceptService } from '../services/concept.service';
 declare var $: any;
@@ -8,22 +8,19 @@ declare var $: any;
   templateUrl: './view-concept.component.html',
   styleUrls: ['./view-concept.component.css']
 })
-export class ViewConceptComponent implements OnChanges {
+export class ViewConceptComponent implements OnInit {
   @Input() 
   concept : Concept;
   @Output() goodPressed = new EventEmitter<boolean>();
   
-  set Concept(concept: Concept) {
-    this.concept = concept;
-  };
   isShowButtonVisible : boolean;
 
   constructor(private conceptService : ConceptService) { 
     this.isShowButtonVisible = true;
   }
 
-  ngOnChanges(changes){
-    console.log(changes);
+  ngOnInit() {
+    this.concept = new Concept();
   }
 
   onClickShow(){
